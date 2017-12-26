efd7852b8d0ee0cd2932c5442d001c75681a0231
Vitor Gaudencio <vitor.oliveira@ecs.com.br>
2017-12-26 15:29:57 -0200

    Aula 8 - Alterando e Deletando registros

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,11 +1,38 @@
-angular.module("alurapic").controller("FotoController", function($scope, $http) {
+angular.module("alurapic").controller("FotoController", function($scope, $http, $routeParams) {
     $scope.foto = {};
     $scope.mensagem = "";
+
+    if ($routeParams.fotoId) {
+        $http.get("/v1/fotos/" + $routeParams.fotoId)
+        .success(function(foto){
+            console.log(foto);
+            $scope.foto = foto;
+        })
+        .error(function(err){
+            console.log(err);
+            $scope.mensagem = "Erro ao buscar foto (" + $routeParams.fotoId +") !";
+        })
+    }
+
     $scope.submeter = function() {
         console.log($scope.foto);
         console.log(this);
 
         if ($scope.formulario.$valid) {
+
+            if ($scope.foto._id) {
+                $http.put("/v1/fotos/" + $scope.foto._id, $scope.foto)
+                .success(function() {
+                    $scope.foto = {};
+                    $scope.mensagem = "Foto alterada com sucesso!";
+                })
+                .error(function(err) {
+                    console.error(err);
+                    $scope.mensagem = "Erro ao alterar!";
+                });
+                return;
+            }
+
             $http.post("/v1/fotos", $scope.foto)
                 .success(function() {
                     $scope.foto = {};
--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,5 +1,7 @@
 angular.module("alurapic").controller("FotosController", function($scope, $http) {
     $scope.fotos =[];
+    $scope.filtro = '';
+    $scope.mensagem = '';
     $http.get("v1/fotos")
         .success(function(d){
             $scope.fotos = d;
@@ -7,5 +9,19 @@ angular.module("alurapic").controller("FotosController", function($scope, $http)
         .error(function(err) {
             console.error(err);
         });
+
+    $scope.remover = function(foto) {
+        $http.delete("/v1/fotos/" + foto._id)
+            .success(function(){
+                var index = $scope.fotos.indexOf(foto);
+                $scope.fotos.splice(index, 1);
+                $scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso!';
+                
+            })
+            .error(function(erro) {
+                console.log(erro);
+                $scope.mensagem = 'Não foi possível apagar a foto ' + foto.titulo;
+            });
+    }
 
 });
