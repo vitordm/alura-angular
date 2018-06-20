5ac70dc340c910b884a3af57c6ca6ddaf0ea2257
Vitor Oliveira <oliveira.vitor3@gmail.com>
2018-06-20 12:52:49 -0300

    Lidando com obervable, listando mensagens, e novos servicos

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,11 +11,17 @@ export class HeroesComponent implements OnInit {
 
   selectedHero : Hero;
 
-  heroes = HEROES;
+  heroes: Hero[];
 
-  constructor() { }
+  constructor(private heroService : HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes() : void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero : Hero) : void {
