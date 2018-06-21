5339858cb7f3d90a372a5113fc3104b5020f4e4e
Vitor Oliveira <oliveira.vitor3@gmail.com>
2018-06-21 15:01:19 -0300

    Estruturando HTTP, Incluindo, Alterando, Deletando e Pesquisando

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -28,4 +28,20 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 
+  add(name : string) : void {
+    name = name.trim();
+    if (!name) return;
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      })
+  }
+
+  delete(hero: Hero) : void {
+    this.heroService.deleteHero(hero)
+      .subscribe(() => {
+        this.heroes = this.heroes.filter(h => h !== hero)
+      });
+  }
+
 }
